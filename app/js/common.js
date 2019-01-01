b6ef00ba23778b41e7b70efe9424ee4ad6ef5078
MilordVladyslav <vladyslav.petrachuk@gmail.com>
2019-01-01 14:14:53 +0200

    1.4

--- app/js/common.js
+++ app/js/common.js
@@ -3,12 +3,10 @@
 const places = document.querySelectorAll('.place')
 
 function appearanceDigitBlock() {
-    const places = document.querySelectorAll('.place')
     const digitDiv = document.createElement('div')
     digitDiv.setAttribute('class', 'digit')
     
-    
-    const GeneratePlace = () =>{
+    const GeneratePlace = () => {
         let putPlace = getRandomPlace(places.length)
         if(places[putPlace].lastChild) { // check if place is placed other digit
             return GeneratePlace() 
@@ -42,7 +40,7 @@ function Movement(keyup) {
 
 
 
-    function MovementDirection(keyup, i, total, min, max) {
+    function MovementDirectionView(keyup, i, total, min, max) {
         c(keyup)
         switch(keyup) {
             case 'ArrowDown':
@@ -72,10 +70,21 @@ function Movement(keyup) {
 
     }
 
+    function MovementDirectionModel(i) {
+        digitDivs[i].parentNode.removeChild(digitDivs[i])
+
+    }
+
+    function RemoveChild (i) {
+        digitDivs[i].parentNode.removeChild(digitDivs[i])
+    }
+    function AddChild () {
+        
+    }
 
     //Логика ниже.
 
-    const max = allPlaces[allPlaces.length -1].classList[1][4]
+    const max = allPlaces[allPlaces.length - 1].classList[1][4]
 
 
         for(const i in digitDivs){
@@ -84,46 +93,43 @@ function Movement(keyup) {
 
                 const current = digitDivs[i].parentNode.classList[1][4]
                 const total = max - current
-                //сюда функцию вхуюжить с проверкой
+                //сюда функцию  с проверкой
                 const min = current - 1
 
-                
+                c(`current - ${current}, total - ${total}, max - ${max}, min - ${min}`) 
 
-                MovementDirection(keyup, i, total, min)
+                MovementDirectionModel(i)
+                MovementDirectionView(keyup, i, total, min)
 
             }else if(keyup === 'ArrowLeft' || keyup === 'ArrowRight'){
                 
                 const current = digitDivs[i].parentNode.classList[2][4]
                 const total = max - current
-                //сюда функцию вхуюжить с проверкой
+                //сюда функцию с проверкой
                 const min = current - 1
-                MovementDirection(keyup, i, total, min)
+                c(`current - ${current}, total - ${total}, max - ${max}, min - ${min}`) 
+                MovementDirectionView(keyup, i, total, min)
     
             }  
         }
     
 }
 
-// vertical movements
-// gorizontal movements
 
-let blya = []
+let keytrigger = []
 
 window.addEventListener('keyup',(e) => {
 
-    blya.push(e.code)
+    keytrigger.push(e.code)
 
-    // blya.push(e.code)
-    //blya.unshift(e.code)
-    c(blya)
-    Movement(blya[0]) 
-    if(blya.length >= 2) {
-        blya = [];
+    c(keytrigger)
+    Movement(keytrigger[0]) 
+    if(keytrigger.length >= 2) {
+        keytrigger = [];
     }
 
     setTimeout(() => {
-        blya = [];
-        c('done')
+        keytrigger = [];
     }, 200);
 })
 
