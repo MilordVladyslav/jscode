76d6331c2f1b5572fa5656a758e98143674592c1
MilordVladyslav <vladyslav.petrachuk@gmail.com>
2019-01-01 23:38:03 +0200

    v.1.6

--- app/js/common.js
+++ app/js/common.js
@@ -5,12 +5,12 @@ const places = document.querySelectorAll('.place')
 function appearanceDigitBlock() {
     const digitDiv = document.createElement('div')
     digitDiv.setAttribute('class', 'digit')
-    
+
     const GeneratePlace = () => {
         let putPlace = getRandomPlace(places.length)
-        if(places[putPlace].lastChild) { // check if place is placed other digit
-            return GeneratePlace() 
-        }else {
+        if (places[putPlace].lastChild) { // check if place is placed other digit
+            return GeneratePlace()
+        } else {
             return putPlace
         }
     }
@@ -19,12 +19,12 @@ function appearanceDigitBlock() {
 }
 
 function getRandomPlace(placesLength) {
-       return 12 
+    return 0
     // return Math.floor(Math.random() * (placesLength));
 }
 
 function IsPlaced(places) {
-    if(!place.hasChildNodes()) return true;
+    if (!place.hasChildNodes()) return true;
 }
 
 // function Isplaced(place) {
@@ -43,30 +43,30 @@ function Movement(keyup) {
 
     function MovementDirectionView(keyup, i, total, min, max) {
         // c(keyup)
-        switch(keyup) {
+        switch (keyup) {
             case 'ArrowDown':
                 // c(keyup)
                 digitDivs[i].classList.remove(`mov-top-${min}00`)
                 digitDivs[i].classList.add(`mov-bottom-${total}00`)
-            break;
+                break;
 
-            case 'ArrowUp': 
+            case 'ArrowUp':
                 // c(keyup)
                 digitDivs[i].classList.remove(`mov-bottom-${total}00`)
                 digitDivs[i].classList.add(`mov-top-${min}00`)
-            break;
+                break;
 
             case 'ArrowLeft':
                 // c(keyup)
                 digitDivs[i].classList.remove(`mov-right-${total}00`)
                 digitDivs[i].classList.add(`mov-left-${min}00`)
-            break;
+                break;
 
             case 'ArrowRight':
                 // c(keyup)
                 digitDivs[i].classList.remove(`mov-left-${min}00`)
                 digitDivs[i].classList.add(`mov-right-${total}00`)
-            break;
+                break;
         }
 
     }
@@ -76,10 +76,10 @@ function Movement(keyup) {
 
     }
 
-    function RemoveChild (i) {
+    function RemoveChild(i) {
         digitDivs[i].parentNode.removeChild(digitDivs[i])
     }
-    function AddChildToBottom (i, total) {
+    function AddChildToBottom(i, total) {
 
         // c(total)
 
@@ -90,12 +90,18 @@ function Movement(keyup) {
         // c(`${parentNodeRow[1][4]} + ${parentNodeRow[2][4]}`)
         // && places[i].classList[2][4] == (places[i].classList[2][4] + total)
         let putPlace = Number(places[i].classList[2][4]) + total - 1;
-        for(var j = places.length; j > 0; j -- ) {
+        for (var j = places.length; j > 0; j--) {
             // c(places[j - 1])
-//  && !places[i].lastChild && digitDivs[i].parentNode != places[i]
-            if(places[j - 1].classList[2][4] == parentNodeCol) {
+            //  && !places[i].lastChild && digitDivs[i].parentNode != places[i]
+            if (places[j - 1].classList[2][4] == parentNodeCol) {
                 c(j)
                 c(places[j - 1])
+            
+                if(!places[j - 1].lastChild) {
+                    digitDivs[i].classList.remove(`mov-bottom-${total}00`)
+                    places[j - 1].appendChild(digitDivs[i])
+                    
+                }
                 // let digitDiv = digitDivs[i]
                 // places[j].appendChild(digitDiv)
                 // c(digitDivs[i])
@@ -115,23 +121,23 @@ function Movement(keyup) {
 
             //     c(place[])
 
-/*                 if(places[i].classList[1][4] == (mumbo + putPlace))  {
-                    c(places[i])
-                } */
-                // c(putPlace)
-                //  c(places[i])
+            /*                 if(places[i].classList[1][4] == (mumbo + putPlace))  {
+                                c(places[i])
+                            } */
+            // c(putPlace)
+            //  c(places[i])
 
-                // c(`janka ${mumbo + putPlace}`)
+            // c(`janka ${mumbo + putPlace}`)
 
 
 
-                // places[i].classList[1][4] + putPlace 
+            // places[i].classList[1][4] + putPlace 
 
-                // places[i].classList.appendChild(digitDivs[i])
-                // c(`total ${total}`)
-                // c(`places ${somevalue}`)
-                // c(`put place - ${putPlace}`)
-                // c(places[i].classList[1] + " - WOW WOW WOW")
+            // places[i].classList.appendChild(digitDivs[i])
+            // c(`total ${total}`)
+            // c(`places ${somevalue}`)
+            // c(`put place - ${putPlace}`)
+            // c(places[i].classList[1] + " - WOW WOW WOW")
             // }
         }
     }
@@ -143,46 +149,50 @@ function Movement(keyup) {
     // [1] - row [2] - col
     // c(test + " - TEST")
 
-        for(const i in digitDivs){
-        
-            if(keyup === 'ArrowDown' || keyup === 'ArrowUp') {
+    for (const i in digitDivs) {
 
-                const current = digitDivs[i].parentNode.classList[1][4]
-                // c(current + "@@@@@@@@@@")
-                const total = max - current
-                //сюда функцию  с проверкой
-                const min = current - 1
+        if (keyup === 'ArrowDown' || keyup === 'ArrowUp') {
 
-                // c(`current - ${current}, total - ${total}, max - ${max}, min - ${min}`) 
+            const current = digitDivs[i].parentNode.classList[1][4]
+            // c(current + "@@@@@@@@@@")
+            const total = max - current
+            //сюда функцию  с проверкой
+            const min = current - 1
 
-                // MovementDirectionModel(i)
+            // c(`current - ${current}, total - ${total}, max - ${max}, min - ${min}`) 
+
+            // MovementDirectionModel(i)
+            setTimeout(() => {
                 AddChildToBottom(i, total)
-                MovementDirectionView(keyup, i, total, min)
+            }, 200);
+            
 
-            }else if(keyup === 'ArrowLeft' || keyup === 'ArrowRight'){
-                
-                const current = digitDivs[i].parentNode.classList[2][4]
-                const total = max - current
-                //сюда функцию с проверкой
-                const min = current - 1
-                // c(`current - ${current}, total - ${total}, max - ${max}, min - ${min}`) 
-                MovementDirectionView(keyup, i, total, min)
-    
-            }  
+            MovementDirectionView(keyup, i, total, min)
+
+        } else if (keyup === 'ArrowLeft' || keyup === 'ArrowRight') {
+
+            const current = digitDivs[i].parentNode.classList[2][4]
+            const total = max - current
+            //сюда функцию с проверкой
+            const min = current - 1
+            // c(`current - ${current}, total - ${total}, max - ${max}, min - ${min}`) 
+            MovementDirectionView(keyup, i, total, min)
+
         }
-    
+    }
+
 }
 
 
 let keytrigger = []
 
-window.addEventListener('keyup',(e) => {
+window.addEventListener('keyup', (e) => {
 
     keytrigger.push(e.code)
 
     // c(keytrigger)
-    Movement(keytrigger[0]) 
-    if(keytrigger.length >= 2) {
+    Movement(keytrigger[0])
+    if (keytrigger.length >= 2) {
         keytrigger = [];
     }
 
